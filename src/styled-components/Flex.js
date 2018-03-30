ac7747e39f29b8eb1b4996d1eec532f50a5c6d96
Garima Harish Bhatia <garima.bhatia@imaginea.com>
2018-03-30 12:34:53 +0530

    changes

--- src/styled-components/Flex.js
+++ src/styled-components/Flex.js
@@ -3,9 +3,9 @@ import styled, { css } from "react-emotion";
 export const Flex = styled("div")`
   display: flex;
   flex-wrap: wrap;
-  flex-direction: row;
-  justify-content: space-between;
-  font-family: roboto;
-  font-size: large;
+  flex-direction: ${props => props.direction || "row"};
+  justify-content: ${props => props.justify || "space-between"};
+  font-family: ${props => props.font || "roboto"};
+  font-size: ${props => props.fontSize || "medium"};
   background-color: ${props => props.color};
 `;
